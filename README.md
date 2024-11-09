a7950e285d87a011a19dca4942e3188f2cfaee77
craterXO <chauonpc@gmail.com>
2024-11-09 00:51:46 -0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,126 @@
+> [!CAUTION]  
+> **⚠️ Plexactyl reaches end-of-life on November 7th, 2024 ⚠️**  
+> With the release of Heliactyl Next, we have decided to discontinue Plexactyl to start creating, Ninovium.
+>  
+> Ever since Ninotainer acquired Plexactyl, we've been waiting for a new major Heliactyl version to start making Ninovium, our next-generation dashboard. Ninovium will be much more polished and independent than Plexactyl ever could've been.  
+<hr>
+<br>
+# Plexactyl • The next generation of dashboards for Pterodactyl
+
+All features:
+- Resource Management (Use it to create servers, etc)
+- Coins (AFK Page earning, Linkvertise earning, Transferring coins)
+- Servers (create, view, edit servers)
+- User System (auth, regen password, etc)
+- Store (buy resources with coins)
+- Dashboard (view resources)
+- Join for Resources (join discord servers for resources, no frontend)
+- Admin (set/add/remove coins & resources)
+- API (for bots & other things)
+
+<hr>
+# Install Guide
+
+**Caution:** Ensure that Pterodactyl is already configured on a domain or else Plexactyl may not function properly.
+
+Access your VPS through SSH and run these Commands:
+
+```bash
+1. sudo apt update -y && sudo apt upgrade -y
+2. sudo apt install -y python3-certbot-nginx
+3. cd /var/www
+4. # Download and unzip the latest Plexactyl release from GitHub into the current folder
+5. curl -sL https://deb.nodesource.com/setup_20.x | sudo -E bash -
+   sudo apt-get install -y nodejs
+   # Customize the config.toml file, specifically updating the panel domain, API key, and Discord authentication settings.
+6. node . # Start Plexactyl. Take a look at "Running in background and on startup" if you want Plexactyl to run in the background
+          # Ctrl + C to stop Plexactyl
+8. sudo ufw allow 80
+9. sudo ufw allow 443
+10. certbot certonly --nginx -d <Your Domain>
+11. nano /etc/nginx/sites-enabled/plexactyl.conf
+12. # Copy the nginx config from Nginx Proxy Config section and replace <domain> with your domain and <port> with the Port Plexactyl is running on 
+    # (You can find the port in the config.toml)
+13. sudo systemctl restart nginx
+14. # Attempt to access your Plexactyl domain
+
+
+# Nginx Proxy Config
+server {
+    listen 80;
+    server_name <domain>;
+    return 301 https://$server_name$request_uri;
+}
+
+server {
+    listen 443 ssl http2;
+
+    location /afk/ws {
+    proxy_http_version 1.1;
+    proxy_set_header Upgrade $http_upgrade;
+    proxy_set_header Connection "upgrade";
+    proxy_pass "http://localhost:<port>/afk/ws";
+    }
+    
+    server_name <domain>;
+    ssl_certificate /etc/letsencrypt/live/<domain>/fullchain.pem;
+    ssl_certificate_key /etc/letsencrypt/live/<domain>/privkey.pem;
+    ssl_session_cache shared:SSL:10m;
+    ssl_protocols SSLv3 TLSv1 TLSv1.1 TLSv1.2;
+    ssl_ciphers  HIGH:!aNULL:!MD5;
+    ssl_prefer_server_ciphers on;
+
+    location / {
+      proxy_pass http://localhost:<port>/;
+      proxy_buffering off;
+      proxy_set_header X-Real-IP $remote_addr;
+  }
+}
+```
+
+<hr>
+# Additional Configuration
+
+#### **Changing the EGG IDs**:
+ Pterodactyl often changes the IDs of the EGGs so you might need to change the IDs in the config.toml to match the Pterodactyl ones
+ You can find the eggs for Minecraft by using `panel.example.com/admin/nests`. Replace panel.example.com with the actual Domain of your Pterodactyl Installation
+
+How to add more eggs:
+1. [Download them from the eggs repository](https://github.com/pelican-eggs/)
+2. Add the eggs to your panel
+3. Get the egg ID of the egg and configure the variables, startup command, egg ID and docker image.
+
+# Updating 
+
+⚠️ database.sqlite compatibility with Dashactyl 0.4 or Plexactyl/Heliactyl 12 has not been tested.
+
+From Heliactyl 14/18 to Plexactyl:
+1. Store certain information such as your api keys, discord auth settings, etc in a .txt file or somewhere safe
+2. Download database.sqlite (This is the Database which includes important data about the user and servers)
+3. Delete all files in the directory of the server (or delete and remake the folder if done in ssh)
+4. Upload the latest Plexactyl release and unzip it
+5. Upload database.sqlite and reconfigure config.toml
+
+Move to a newer Plexactyl release:
+1. Delete everything except config.toml, database.sqlite
+2. Upload the latest Plexactyl release and unzip it
+3. reconfigure config.toml and upload your old database.sqlite
+4. All done now start Plexactyl again
+
+# Running in background and on startup
+Installing [pm2](https://github.com/Unitech/pm2):
+- Run `npm install pm2 -g` on the vps
+
+Starting the Dashboard in Background:
+- Change directory to your Plexactyl folder Using `cd` command, Example: `cd /var/www/plexactyl` 
+- To run Plexactyl, use `pm2 start index.js --name "Plexactyl"`
+- To view logs, run `pm2 logs Plexactyl`
+
+Making the dashboard runs on startup:
+- Make sure your dashboard is running in the background with the help of [pm2](https://github.com/Unitech/pm2)
+- You can check if Plexactyl is running in background with `pm2 list`
+- Once you confirmed that Plexactyl is running in background, you can create a startup script by running `pm2 startup` and `pm2 save`
+- Note: Supported init systems are `systemd`, `upstart`, `launchd`, `rc.d`
+- To stop your Plexactyl from running in the background, use `pm2 unstartup`
+
+To stop a currently running Plexactyl instance, use `pm2 stop Plexactyl`
---
+++ app.js
@@ -0,0 +1,315 @@
+"use strict";
+
+// Load packages.
+const fs = require("fs");
+const chalk = require("chalk");
+const arciotext = require("./handlers/afk.js");
+const cluster = require("cluster");
+const chokidar = require('chokidar');
+
+global.Buffer = global.Buffer || require("buffer").Buffer;
+
+if (typeof btoa === "undefined") {
+  global.btoa = function (str) {
+    return Buffer.from(str, "binary").toString("base64");
+  };
+}
+if (typeof atob === "undefined") {
+  global.atob = function (b64Encoded) {
+    return Buffer.from(b64Encoded, "base64").toString("binary");
+  };
+}
+
+// Load settings.
+const loadConfig = require("./handlers/config");
+const settings = loadConfig("./config.toml");
+
+
+const defaultthemesettings = {
+  index: "index.ejs",
+  notfound: "index.ejs",
+  redirect: {},
+  pages: {},
+  mustbeloggedin: [],
+  variables: {},
+};
+
+/**
+ * Renders data for the theme.
+ * @param {Object} req - The request object.
+ * @param {Object} theme - The theme object.
+ * @returns {Promise<Object>} The rendered data.
+ */
+async function renderdataeval(req, theme) {
+  const JavaScriptObfuscator = require('javascript-obfuscator');
+  let settings = loadConfig("./config.toml");
+  let renderdata = {
+    req: req,
+    settings: settings,
+    userinfo: req.session.userinfo,
+    queued: (req.session.userinfo ? await db.get(req.session.userinfo.id + '-queued') : { }),
+    packagename: req.session.userinfo ? await db.get("package-" + req.session.userinfo.id) ? await db.get("package-" + req.session.userinfo.id) : settings.api.client.packages.default : null,
+    extraresources: !req.session.userinfo ? null : (await db.get("extra-" + req.session.userinfo.id) ? await db.get("extra-" + req.session.userinfo.id) : {
+      ram: 0,
+      disk: 0,
+      cpu: 0,
+      servers: 0
+    }),
+    packages: req.session.userinfo ? settings.api.client.packages.list[await db.get("package-" + req.session.userinfo.id) ? await db.get("package-" + req.session.userinfo.id) : settings.api.client.packages.default] : null,
+    coins: settings.api.client.coins.enabled == true ? (req.session.userinfo ? (await db.get("coins-" + req.session.userinfo.id) ? await db.get("coins-" + req.session.userinfo.id) : 0) : null) : null,
+    bal: (req.session.userinfo ? (await db.get("bal-" + req.session.userinfo.id) ? await db.get("bal-" + req.session.userinfo.id) : 0) : null),
+    pterodactyl: req.session.pterodactyl,
+    extra: theme.settings.variables,
+    db: db
+  };
+  renderdata.arcioafktext = JavaScriptObfuscator.obfuscate(`
+    let everywhat = ${settings.api.afk.every};
+    let gaincoins = ${settings.api.afk.coins};
+    let wspath = "ws";
+
+    ${arciotext}
+  `).getObfuscatedCode();
+  return renderdata;
+}
+
+module.exports.renderdataeval = renderdataeval;
+
+// Load database
+const Database = require("keyv");
+const db = new Database(settings.database);
+
+module.exports.db = db;
+
+if (cluster.isMaster) {
+  // Display loading spinner
+  let spinnerFrames = ['-', '\\', '|', '/'];
+  let currentFrame = 0;
+  
+  const spinner = setInterval(() => {
+    process.stdout.write(chalk.gray('\r' + spinnerFrames[currentFrame++] + ' Working on it...'));
+    currentFrame %= spinnerFrames.length;
+  }, 100);
+  
+  setTimeout(() => {
+    clearInterval(spinner);
+    process.stdout.write('\r');
+    startApp();
+  }, 3000);
+
+  function startApp() {
+    // Create tree view of modules in /modules/
+    let moduleFiles = fs.readdirSync("./modules").filter((file) => file.endsWith(".js"));
+    const settingsVersion = settings.version;
+  
+    console.log(chalk.gray("Loading modules tree..."));
+    console.log(chalk.gray("Version: " + settingsVersion));
+
+    let modulesTable = [];
+
+    moduleFiles.forEach(file => {
+      const module = require('./modules/' + file);
+      if (!module.load || !module.plexactylModule) {
+        modulesTable.push({ File: file, Name: 'Unknown', 'Target Platform': 'Unknown' });
+        return;
+      }
+    
+      const { name, target_platform } = module.plexactylModule;
+  
+      modulesTable.push({ File: file, Name: name, 'Target Platform': target_platform });
+    });
+
+    console.table(modulesTable);
+  
+    const numCPUs = settings.clusters;
+    console.log(chalk.gray('Starting workers on Plexactyl ' + settings.version));
+    console.log(chalk.gray(`Master ${process.pid} is running`));
+    console.log(chalk.gray(`Forking ${numCPUs} workers...`));
+  
+    if (numCPUs > 48 || numCPUs < 1) {
+      console.log(chalk.red('Error: Clusters amount was either below 1, or above 48.'))
+      process.exit()
+    }
+
+    for (let i = 0; i < numCPUs; i++) {
+      cluster.fork();
+    }
+  
+    cluster.on('exit', (worker, code, signal) => {
+      console.log(chalk.red(`Worker ${worker.process.pid} died. Forking a new worker...`));
+      cluster.fork();
+    });
+    
+    // Watch for file changes and reboot workers
+    const watcher = chokidar.watch('./modules');
+    watcher.on('change', (path) => {
+      console.log(chalk.yellow(`File changed: ${path}. Rebooting workers...`));
+      for (const id in cluster.workers) {
+        cluster.workers[id].kill();
+      }
+    });
+
+    // Watch for file changes and reboot workers
+    const watcher2 = chokidar.watch('./config.toml');
+    watcher2.on('change', (path) => {
+      console.log(chalk.yellow(`File changed: ${path}. Rebooting workers...`));
+      for (const id in cluster.workers) {
+        cluster.workers[id].kill();
+      }
+    });
+  }
+  
+  cluster.on('online', (worker) => {
+    const workerTree = Object.values(cluster.workers).map(worker => ({
+      id: worker.id,
+      pid: worker.process.pid,
+      state: worker.state,
+    }));
+    console.log(chalk.gray('Current workers status:'));
+    console.table(workerTree);
+  });
+
+} else {
+  // Load websites.
+  const express = require("express");
+  const app = express();
+  app.set('view engine', 'ejs');
+  require("express-ws")(app);
+
+  // Load express addons.
+  const session = require("express-session");
+  const SessionStore = require("./handlers/session");
+  const indexjs = require("./app.js");
+  let secret = (Math.random() + 1).toString(36).substring(7);
+
+  // Load the website.
+  module.exports.app = app;
+
+  app.use((req, res, next) => {
+    res.setHeader("X-Powered-By", "Plexactyl");
+    next();
+  });
+
+  app.use(
+    session({
+      store: new SessionStore({ uri: settings.database }),
+      secret: secret,
+      resave: false,
+      saveUninitialized: false,
+      cookie: { secure: false }, // Set to true if using https
+    })
+  );
+
+  app.use(
+    express.json({
+      inflate: true,
+      limit: "500kb",
+      reviver: null,
+      strict: true,
+      type: "application/json",
+      verify: undefined,
+    })
+  );
+
+  const listener = app.listen(settings.website.port, async function () {
+    /* clear all afk sessions */
+    await db.set('afkSessions', {});
+    console.log(
+      chalk.white("Web cluster is now ") + chalk.green('online')
+    );
+  });
+
+  var cache = false;
+  app.use(function (req, res, next) {
+    let manager = loadConfig("./config.toml").api
+      .client.ratelimits;
+    if (manager[req._parsedUrl.pathname]) {
+      if (cache == true) {
+        setTimeout(async () => {
+          let allqueries = Object.entries(req.query);
+          let querystring = "";
+          for (let query of allqueries) {
+            querystring = querystring + "&" + query[0] + "=" + query[1];
+          }
+          querystring = "?" + querystring.slice(1);
+          res.redirect(
+            (req._parsedUrl.pathname.slice(0, 1) == "/"
+              ? req._parsedUrl.pathname
+              : "/" + req._parsedUrl.pathname) + querystring
+          );
+        }, 1000);
+        return;
+      } else {
+        cache = true;
+        setTimeout(async () => {
+          cache = false;
+        }, 1000 * manager[req._parsedUrl.pathname]);
+      }
+    }
+    next();
+  });
+// Load the API files
+let apifiles = fs.readdirSync("./modules").filter((file) => file.endsWith(".js"));
+
+apifiles.forEach((file) => {
+  let apifile = require(`./modules/${file}`);
+  
+  // Check if the module has a 'load' function and call it if it exists
+  if (typeof apifile.load === 'function') {
+    apifile.load(app, db);
+  } else {
+    console.error(`The module ${file} does not export a 'load' function.`);
+  }
+});
+
+
+  app.all("*", async (req, res) => {
+    if (req.session.pterodactyl)
+      if (
+        req.session.pterodactyl.id !==
+        (await db.get("users-" + req.session.userinfo.id))
+      )
+        return res.redirect("/cp/login?prompt=none");
+    let theme = indexjs.get(req);
+    let settings = loadConfig("./config.toml");
+    if (settings.api.afk.enabled == true)
+      req.session.arcsessiontoken = Math.random().toString(36).substring(2, 15);
+    if (theme.settings.mustbeloggedin.includes(req._parsedUrl.pathname))
+      if (!req.session.userinfo || !req.session.pterodactyl)
+        return res.redirect(
+          "/cp/login" +
+            (req._parsedUrl.pathname.slice(0, 1) == "/"
+              ? "?redirect=" + req._parsedUrl.pathname.slice(1)
+              : "")
+        );
+    const data = await renderdataeval(req, theme);
+    res.render(theme.settings.pages[req._parsedUrl.pathname.slice(1)] || theme.settings.notfound, data);
+  });
+
+  module.exports.get = function (req) {
+    return {
+      settings: fs.existsSync(`./views/pages.json`)
+        ? JSON.parse(fs.readFileSync(`./views/pages.json`).toString())
+        : defaultthemesettings
+    };
+  };
+
+  module.exports.islimited = async function () {
+    return cache == true ? false : true;
+  };
+
+  module.exports.ratelimits = async function (length) {
+    if (cache == true) return setTimeout(indexjs.ratelimits, 1);
+    cache = true;
+    setTimeout(async function () {
+      cache = false;
+    }, length * 1000);
+  };
+
+  process.on('uncaughtException', (error) => {
+    console.error('Uncaught Exception:', error);
+  });
+
+  process.on('unhandledRejection', (reason, promise) => {
+    console.error('Unhandled Rejection at:', promise, 'reason:', reason);
+  });
+}
